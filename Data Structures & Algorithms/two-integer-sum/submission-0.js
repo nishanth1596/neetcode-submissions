7724a2c1514ceadf776c15949f7305bec3047525
nishanth1596 <121683297+nishanth1596@users.noreply.github.com>
2026-08-17 18:49:45 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = {};
+
+        for (let i = 0; i < nums.length; i++) {
+            if (map[target - nums[i]] === undefined) {
+                map[nums[i]] = i;
+            } else {
+                return [map[target - nums[i]], i];
+            }
+        }
+    }
+}
